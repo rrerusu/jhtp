bbeec33188e96307a62bb033171586501d55cb69
Ruk <rrerusu@gmail.com>
2020-10-15 13:27:43 -0500

    Print the list of shuffled cards

---
+++ chapter7/src/org/erusu/jhtp/chapter7/examples/playing_cards/DeckOfCardsTest.java
@@ -0,0 +1,15 @@
+package org.erusu.jhtp.chapter7.examples.playing_cards;
+
+public class DeckOfCardsTest {
+	public static void main(String[] args) {
+		DeckOfCards myDeck = new DeckOfCards();
+		myDeck.shuffle();
+		
+		for(int i = 1; i <= 52; i++) {
+			System.out.printf("%-19s", myDeck.dealCard());
+			
+			if(i % 4 == 0)
+				System.out.println();
+		}
+	}
+}
