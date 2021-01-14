aa1762e21afa632f8c00e58ec4b576a9d8a2fd6a
Ruk <rrerusu@gmail.com>
2021-01-14 12:16:56 -0600

    Fibonacci Sequence: using doubles to see how many numbers we can print

--- chapter7/src/org/erusu/jhtp/chapter7/exercises/FibonacciSequence.java
+++ chapter7/src/org/erusu/jhtp/chapter7/exercises/FibonacciSequence.java
@@ -2,25 +2,31 @@ package org.erusu.jhtp.chapter7.exercises;
 
 public class FibonacciSequence {
 	public static void main(String[] args) {
-		int term = 2;
+		int term = 1;
 		
-		while(term < 49) {
-			System.out.printf("%d: %d%n", term - 1, fibonacci(term));
+		while(term < 1479) {
+			System.out.printf("%d: %.0f%n", term, fibonacci(term));
 			term++;
 		}
 	}
 	
 	// calculate the nth term of the fibonacci sequence
-	public static int fibonacci(int n) {
-		int[] seq = new int[n];
+	public static double fibonacci(int n) {
+		double[] seq = new double[n];
 		
-		seq[0] = 0;
-		seq[1] = 1;
-		
-		for(int indx = 2; indx < seq.length; indx++) {
-			seq[indx] = seq[indx - 1] + seq[indx - 2];
+		if(n == 1)
+			return 0;
+		else if(n == 2)
+			return 1;
+		else {
+			seq[0] = 0;
+			seq[1] = 1;
+			
+			for(int indx = 2; indx < seq.length; indx++) {
+				seq[indx] = seq[indx - 1] + seq[indx - 2];
+			}
+			
+			return seq[n - 1];
 		}
-		
-		return seq[n - 1];
 	}
 }
