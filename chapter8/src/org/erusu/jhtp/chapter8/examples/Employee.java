9135278d5d2354f10dbf3bbf89b234f2c2354907
rrerusu <rrerusu@gmail.com>
2021-05-07 14:28:16 -0500

    Time2 and Driver, and Date, Employee, and Driver

---
+++ chapter8/src/org/erusu/jhtp/chapter8/examples/Employee.java
@@ -0,0 +1,19 @@
+package org.erusu.jhtp.chapter8.examples;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Date hireDate;
+
+    public Employee(String first, String last, Date dateOfBirth, Date dateOfHire) {
+        firstName = first;
+        lastName = last;
+        birthDate = dateOfBirth;
+        hireDate = dateOfHire;
+    }
+
+    public String toString() {
+        return String.format("%s, %s    Hired: %s   Birthday: %s", lastName, firstName, hireDate, birthDate);
+    }
+}
