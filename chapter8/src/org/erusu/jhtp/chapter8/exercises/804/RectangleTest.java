e2e17fc8a0f15a75704122dca523cd7372d0cae2
rrerusu <rrerusu@gmail.com>
2021-05-10 14:48:42 -0500

    First exercise: Basic class definitions, get and set methods, and variable checking

---
+++ chapter8/src/org/erusu/jhtp/chapter8/exercises/804/RectangleTest.java
@@ -0,0 +1,13 @@
+package org.erusu.jhtp.chapter8.exercises._804;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle myRectangle = new Rectangle();
+
+        System.out.printf("Length: %.2f%nWidth: %.2f%nPerimeter: %.2f%nArea: %.2f%n", 
+        myRectangle.getLength(), myRectangle.getWidth(),
+        myRectangle.getPerimeter(), myRectangle.getArea());
+
+        myRectangle.setWidth(200);
+    }
+}
