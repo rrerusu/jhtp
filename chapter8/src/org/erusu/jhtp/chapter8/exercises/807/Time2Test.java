b61379c32e0b5f2f64e747cba3d7635614a2ca38
rrerusu <rrerusu@gmail.com>
2021-05-14 22:24:04 -0500

    Time2 exercise: Make time roll over from 11:59:59 to 00:00:00

---
+++ chapter8/src/org/erusu/jhtp/chapter8/exercises/807/Time2Test.java
@@ -0,0 +1,48 @@
+package org.erusu.jhtp.chapter8.exercises._807;
+
+public class Time2Test {
+    public static void main(String[] args) {
+        Time2 t1 = new Time2();
+        Time2 t2 = new Time2(2);
+        Time2 t3 = new Time2(21, 34);
+        Time2 t4 = new Time2(12, 25, 42);
+        Time2 t5 = new Time2(t4);
+        Time2 t7 = new Time2(23, 59, 59);
+
+        System.out.println("Constructed with:");
+        System.out.println("t1: all arguments defaulted");
+        System.out.printf("    %s%n", t1.toUniversalString());
+        System.out.printf("    %s%n", t1.toString());
+
+        System.out.println("t2: hour specified: minute and second defaulted");
+        System.out.printf("    %s%n", t2.toUniversalString());
+        System.out.printf("    %s%n", t2.toString());
+
+        System.out.println("t3: hour and minute specified: second defaulted");
+        System.out.printf("    %s%n", t3.toUniversalString());
+        System.out.printf("    %s%n", t3.toString());
+
+        System.out.println("t4: hour, minute, and second specified");
+        System.out.printf("    %s%n", t4.toUniversalString());
+        System.out.printf("    %s%n", t4.toString());
+
+        System.out.println("t5: Time2 object t4 specified");
+        System.out.printf("    %s%n", t5.toUniversalString());
+        System.out.printf("    %s%n", t5.toUniversalString());
+
+        try {
+            Time2 t6 = new Time2(27, 74, 99);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("%nException while initializing t6: %s%n", e.getMessage());
+        }
+
+        System.out.println("t7: Time2 object t7 specified");
+        System.out.printf("    %s%n", t7.toUniversalString());
+        System.out.printf("    %s%n", t7.toUniversalString());
+
+        t7.tick();
+        System.out.println("t7: Time2 object t7 specified");
+        System.out.printf("    %s%n", t7.toUniversalString());
+        System.out.printf("    %s%n", t7.toUniversalString());
+    }
+}
