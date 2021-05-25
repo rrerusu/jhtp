c69b0e1700023e81afb657b3b6e668a3df8d8443
rrerusu <rrerusu@gmail.com>
2021-05-24 19:14:51 -0500

    Fixed all conditional methods

--- chapter8/src/org/erusu/jhtp/chapter8/exercises/_816/HugeInteger.java
+++ chapter8/src/org/erusu/jhtp/chapter8/exercises/_816/HugeInteger.java
@@ -1,6 +1,8 @@
 package org.erusu.jhtp.chapter8.exercises._816;
 
 public class HugeInteger {
+	static final int HUGE_INTEGER_LIMIT = 40;
+
     private final int[] num;
     private boolean positive;
 
@@ -18,7 +20,7 @@ public class HugeInteger {
      * @param numArr Array to pass into num value
      */
     public HugeInteger(int[] numArr) {
-        num = new int[40];
+        num = new int[HUGE_INTEGER_LIMIT];
 
         for(int nextDigit = 1; nextDigit <= numArr.length; nextDigit++) {
             num[num.length - nextDigit] = numArr[numArr.length - nextDigit];
@@ -33,16 +35,33 @@ public class HugeInteger {
      * @param positiveSign Sign value of number: positive or negative
      */
     public HugeInteger(int[] numArr, boolean positiveSign) {
-        num = new int[40];
+        num = new int[HUGE_INTEGER_LIMIT];
 
         positive = positiveSign;
 
         for(int nextDigit = 1; nextDigit <= numArr.length; nextDigit++) {
             num[num.length - nextDigit] = numArr[numArr.length - nextDigit];
         }
-
-        positive = true;
     }
+
+	/**
+	 * Constructor: Set this number and sign to given value and sign
+	 * @param newNum number to change this number to
+	 */
+	public HugeInteger(HugeInteger newNum) {
+		num = newNum.getNum();
+		positive = newNum.isPositive();
+	}
+
+	/**
+	 * Constructor: Set this number and sign to given value and sign
+	 * @param newNum number to change this number to
+	 * @param newSign new sign to change number to
+	 */
+	public HugeInteger(HugeInteger newNum, boolean newSign) {
+		num = newNum.getNum();
+		positive = newSign;
+	}
 
     // Accessor and Modifier Methods
     /**
@@ -70,6 +89,14 @@ public class HugeInteger {
         return positive;
     }
 
+	/**
+	 * Modifier method for positive
+	 * @param sign Positive or negative sign of number
+	 */
+	public void setSign(boolean sign) {
+		positive = sign;
+	}
+
     /**
      * Book Method: Parse String and create HugeInteger from it
      * @param strNum string to convert into num array
@@ -95,15 +122,15 @@ public class HugeInteger {
      * @return If this num is equivalent to anotherNum
      */
     public boolean isEqualTo(HugeInteger anotherNum) {
-        if(positive != anotherNum.isPositive())
-            return false;
+    	if(positive != anotherNum.isPositive())
+    		return false;
 
-        for(int digit = 0; digit < num.length - 1; digit++) {
-            if(num[digit] != anotherNum.getNum()[digit])
-                return false;
-        }
+    	for(int indx = 0; indx < HUGE_INTEGER_LIMIT; indx++) {
+    		if(num[indx] != anotherNum.getNum()[indx])
+    			return false;
+		}
 
-        return true;
+    	return true;
     }
 
     /**
@@ -112,15 +139,7 @@ public class HugeInteger {
      * @return If this num is not equivalent to anotherNum
      */
     public boolean isNotEqualTo(HugeInteger anotherNum) {
-        if(positive == anotherNum.isPositive())
-            return false;
-
-        for(int digit = 0; digit < num.length - 1; digit++) {
-            if(num[digit] == anotherNum.getNum()[digit])
-                return false;
-        }
-
-        return true;
+    	return !this.isEqualTo(anotherNum);
     }
 
     /**
@@ -132,43 +151,55 @@ public class HugeInteger {
         if(positive ^ anotherNum.isPositive())
             return anotherNum.isPositive();
 
-        for(int digit = 0; digit < num.length - 1; digit++) {
-            if((positive && num[digit] > anotherNum.getNum()[digit]) ||
-                (!positive && num[digit] < anotherNum.getNum()[digit]))
-                    return false;
+        for(int digit = 0; digit < HUGE_INTEGER_LIMIT; digit++) {
+            if((positive && anotherNum.getNum()[digit] > num[digit]) ||
+                (!positive && anotherNum.getNum()[digit] < num[digit]))
+                    return true;
+			if((positive && anotherNum.getNum()[digit] < num[digit]) ||
+				(!positive && anotherNum.getNum()[digit] > num[digit]))
+				return false;
         }
 
-        return true;
+        return false;
     }
 
-    // TODO: isLessThanOrEqualTo()
 	/**
 	 * Book Method: Test to see if this HugeInteger is less than or equal to another HugeInteger
 	 * @param anotherNum num to test against
 	 * @return Is this num less than or equal to anotherNum
 	 */
 	public boolean isLessThanOrEqualTo(HugeInteger anotherNum) {
-
+		return !this.isGreaterThan(anotherNum);
 	}
 
-	// TODO: isGreaterThan()
 	/**
 	 * Book Method: Test to see if this HugeInteger is greater than another HugeInteger
 	 * @param anotherNum num to test against
 	 * @return Is this num greater than anotherNum
 	 */
 	public boolean isGreaterThan(HugeInteger anotherNum) {
+		if(positive ^ anotherNum.isPositive())
+			return positive;
 
+		for(int digit = 0; digit < HUGE_INTEGER_LIMIT; digit++) {
+			if((positive && num[digit] < anotherNum.getNum()[digit]) ||
+				(!positive && num[digit] > anotherNum.getNum()[digit]))
+				return false;
+			if((positive && num[digit] > anotherNum.getNum()[digit]) ||
+				(!positive && num[digit] < anotherNum.getNum()[digit]))
+				return true;
+		}
+
+		return false;
 	}
 
-	// TODO: isGreaterThanOrEqualTo()
 	/**
 	 * Book Method: Test to see if this HugeInteger is greater than or equal to another HugeInteger
 	 * @param anotherNum num to test against
 	 * @return Is this num greater than or equal to anotherNum
 	 */
 	public boolean isGreaterThanOrEqualTo(HugeInteger anotherNum) {
-
+		return !this.isLessThan(anotherNum);
 	}
 
 	/**
@@ -185,18 +216,35 @@ public class HugeInteger {
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 */
 
-	// TODO: add()
 	/**
 	 * Add 2 HugeIntegers
 	 * @param num1 first HugeInteger
 	 * @param num2 second HugeInteger
-	 * @return sum of num1 and num2
+	 * @return sum of num1 and num2.  If sum is too big, print error message and return nothing
 	 */
 	public static HugeInteger add(HugeInteger num1, HugeInteger num2) {
+		if(num1.isPositive() ^ num2.isPositive()) {
+			return (num1.isPositive()) ? HugeInteger.subtract(num1, new HugeInteger(num2, true)) :
+				HugeInteger.subtract(num2, new HugeInteger(num1, true));
+		} else {
+			int[] sum = new int[HUGE_INTEGER_LIMIT];
+			try {
+				int digitSum,
+					memory = 0;
+				for(int indx = 1; indx < HUGE_INTEGER_LIMIT; indx++) {
+					digitSum = num1.getNum()[HUGE_INTEGER_LIMIT - indx] + num2.getNum()[HUGE_INTEGER_LIMIT - indx] + memory;
+					sum[HUGE_INTEGER_LIMIT - indx] = digitSum % 10;
+					memory = digitSum / 10;
+				}
+			} catch (ArrayIndexOutOfBoundsException sumTooBig) {
+				System.out.printf("Error: %s%n%n", sumTooBig.toString());
+				return null;
+			}
 
+			return new HugeInteger(sum, num1.isPositive());
+		}
 	}
 
-	// TODO: subtract()
 	/**
 	 * Subtract 2 HugeIntegers
 	 * @param num1 first HugeInteger
@@ -204,7 +252,31 @@ public class HugeInteger {
 	 * @return difference of num1 and num2
 	 */
 	public static HugeInteger subtract(HugeInteger num1, HugeInteger num2) {
+		if(num1.isPositive() ^ num2.isPositive()) {
+			return HugeInteger.add(num1, new HugeInteger(num2, !num2.isPositive()));
+		} else {
+			if(num1.isEqualTo(num2))
+				return new HugeInteger();
+			else {
+				int[] difference = new int[HUGE_INTEGER_LIMIT];
+				try {
+					int num1Digit, num2Digit, nextDigit;
+					boolean carry = false;
+					for(int indx = 1; indx < HUGE_INTEGER_LIMIT; indx++) {
+						nextDigit = HUGE_INTEGER_LIMIT - indx;
+						num1Digit = num1.getNum()[nextDigit];
+						num2Digit = num2.getNum()[nextDigit];
+						difference[nextDigit] = (num1Digit + 10 - num2Digit) % 10 - ((carry) ? 1 : 0);
+						carry = num1Digit < num2Digit;
+					}
+				} catch (ArrayIndexOutOfBoundsException differenceTooBig) {
+					System.out.printf("Error: %s%n%n", differenceTooBig.toString());
+					return null;
+				}
 
+				return new HugeInteger(difference, num1.isGreaterThan(num2));
+			}
+		}
 	}
 
 	// TODO: multiply()
@@ -214,9 +286,9 @@ public class HugeInteger {
 	 * @param num2 second HugeInteger
 	 * @return product of num1 and num2
 	 */
-	public static HugeInteger multiply(HugeInteger num1, HugeInteger num2) {
-
-	}
+//	public static HugeInteger multiply(HugeInteger num1, HugeInteger num2) {
+//
+//	}
 
 	// TODO: divide()
 	/**
@@ -225,9 +297,9 @@ public class HugeInteger {
 	 * @param num2 second HugeInteger
 	 * @return quotient of num1 and num2
 	 */
-	public static HugeInteger divide(HugeInteger num1, HugeInteger num2) {
-
-	}
+//	public static HugeInteger divide(HugeInteger num1, HugeInteger num2) {
+//
+//	}
 
 	// TODO: remainder()
 	/**
@@ -236,9 +308,9 @@ public class HugeInteger {
 	 * @param num2 second HugeInteger
 	 * @return remainder of num1 and num2
 	 */
-	public static HugeInteger remainder(HugeInteger num1, HugeInteger num2) {
-
-	}
+//	public static HugeInteger remainder(HugeInteger num1, HugeInteger num2) {
+//
+//	}
 
     /**
      * toString Override: Create 40-digit long number with sign
@@ -246,10 +318,12 @@ public class HugeInteger {
      */
     @Override
     public String toString() {
-        String str = positive ? "" : "-";
+        String str = "";
         for(int digit : num)
             str += digit;
 
-        return str;
+        str = (positive ? "" : "-") + str.replaceAll("^([0]+)", "");
+
+        return (str.equals("")) ? "0" : str;
     }
 }
--- chapter8/src/org/erusu/jhtp/chapter8/exercises/_816/HugeIntegerTest.java
+++ chapter8/src/org/erusu/jhtp/chapter8/exercises/_816/HugeIntegerTest.java
@@ -4,16 +4,60 @@ package org.erusu.jhtp.chapter8.exercises._816;
 
 public class HugeIntegerTest {
     public static void main(String[] args) {
-        HugeInteger num1 = new HugeInteger(),
-            num2 = new HugeInteger();
+        HugeInteger test1num1 = new HugeInteger(), test1num2 = new HugeInteger(),
+			test2num1 = new HugeInteger(), test2num2 = new HugeInteger(),
+			test3num1 = new HugeInteger(), test3num2 = new HugeInteger(),
+			test3num3 = new HugeInteger(), test3num4 = new HugeInteger(),
+			zeroTestNum = new HugeInteger();
 
-        num1.parse("-2");
-        num2.parse("-2");
-        System.out.printf("num1: %s%nnum2: %s%n", num1.toString(), num2.toString());
+		String[][] conditionalResults = new String[8][8];
 
-        // TODO: implement other test cases
-		System.out.printf("num1 = num2: %b%nnum1 != num2: %b%n", num1.isEqualTo(num2), num1.isNotEqualTo(num2));
-        System.out.printf("num1 < num2: %b%nnum1 <= num2: %b%n", num1.isLessThan(num2), num1.isLessThanOrEqualTo(num2));
-		System.out.printf("num1 > num2: %b%nnum1 >= num2: %b%n", num1.isGreaterThan(num2), num1.isGreaterThanOrEqualTo(num2));
+        // create test cases
+        test1num1.parse("10");
+		test1num2.parse("5");
+		test2num1.parse("-5");
+		test2num2.parse("-10");
+		test3num1.parse("2");
+		test3num2.parse("-2");
+		test3num3.parse("-3");
+		test3num4.parse("-3");
+		zeroTestNum.parse("0");
+
+		// set values
+		conditionalResults[0] = runTests(test1num1, test1num2);
+		conditionalResults[1] = runTests(test1num2, test1num1);
+		conditionalResults[2] = runTests(test2num1, test2num2);
+		conditionalResults[3] = runTests(test2num2, test2num1);
+
+		conditionalResults[4] = runTests(test3num1, test3num2);
+		conditionalResults[5] = runTests(test3num2, test3num1);
+		conditionalResults[6] = runTests(test3num3, test3num3);
+		conditionalResults[7] = runTests(test3num4, test3num4);
+
+		System.out.printf("Conditional Method Tests:%n| %40s | %40s | %10s | %10s | %10s | %10s | %10s | %10s |%n",
+			"num 1", "num 2", "==", "!=", "<", "<=", ">", ">=");
+		System.out.printf("%s%s%n",
+			"-------------------------------------------------------------------------------------",
+			"--------------------------------------------------------------------------------");
+
+		for (String[] conditionalResult : conditionalResults) {
+			System.out.printf("| %40s | %40s | %10s | %10s | %10s | %10s | %10s | %10s |%n",
+				conditionalResult[0], conditionalResult[1], conditionalResult[2],
+				conditionalResult[3], conditionalResult[4], conditionalResult[5],
+				conditionalResult[6], conditionalResult[7]);
+		}
+
+		System.out.printf("%n0 integer test:%n0 == 0? -> %s%n5 == 0? -> %s%n%n%n",
+			zeroTestNum.isZero(), test1num2.isZero());
+
+		HugeInteger test = HugeInteger.add(test1num1, test1num2);
+
+		System.out.printf("num1 + num2 = %s%nnum1 - num2 = %s%n", HugeInteger.add(test1num1, test1num2).toString(), HugeInteger.subtract(test1num1, test1num2).toString());
     }
+
+    public static String[] runTests(HugeInteger num1, HugeInteger num2) {
+		return new String[]{num1.toString(), num2.toString(), "" + num1.isEqualTo(num2), "" + num1.isNotEqualTo(num2),
+			"" + num1.isLessThan(num2), "" + num1.isLessThanOrEqualTo(num2),
+			"" + num1.isGreaterThan(num2), "" + num1.isGreaterThanOrEqualTo(num2)};
+	}
 }
