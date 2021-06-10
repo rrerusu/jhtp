53bb5ebc1af9c39751838162dbb21d99a13f5e20
rrerusu <rrerusu@gmail.com>
2021-06-09 19:03:33 -0500

    Access subclass method through superclass object

---
+++ chapter10/src/org/erusu/jhtp/chapter10/exercises/PolymorphismTest.java
@@ -0,0 +1,22 @@
+package org.erusu.jhtp.chapter10.exercises;
+
+public class PolymorphismTest {
+	public static void main(String[] args) {
+		CommissionEmployee commissionEmployee = new CommissionEmployee("Sue", "Jones",
+				"222-22-2222", 10000, 0.06);
+		BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee(
+				"Bob", "Lewis", "333-33-3333", 5000, 0.04, 300);
+
+		System.out.printf("%s %s:\n\n%s\n\n", "Call CommissionEmployee's toString with superclass reference",
+			"to superclass object", commissionEmployee.toString());
+		System.out.printf("%s %s:\n\n%s\n\n",
+			"Call BasePlusCommissionEmployee's toString with subClass", "reference to subclass object",
+			basePlusCommissionEmployee.toString());
+
+		CommissionEmployee commissionEmployee2 = basePlusCommissionEmployee;
+
+		System.out.printf("%s %s:\n\n%s\n\n",
+			"Call BasePlusCommissionEmployee's toString with superclass",
+			"reference to subclass object", commissionEmployee2.toString());
+	}
+}
