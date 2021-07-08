00314062b783defc16c40040f4c77c130ddbf46a
rrerusu <rrerusu@gmail.com>
2021-07-08 17:19:33 -0500

    Substrings

---
+++ src/org/erusu/jhtp/chapter14/examples/SubString.java
@@ -0,0 +1,11 @@
+package org.erusu.jhtp.chapter14.examples;
+
+public class SubString {
+    public static void main(String[] args) {
+        String letters = "abcdefghijklmabcdefghijklm";
+
+        System.out.printf("Substring from index 20 to end is \"%s\"\n", letters.substring(20));
+        System.out.printf("%s \"%s\"\n", "Substring from index 3 up to, but not including 6 is",
+            letters.substring(3, 6));
+    }
+}
