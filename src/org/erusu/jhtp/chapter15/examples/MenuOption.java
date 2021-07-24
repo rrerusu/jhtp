0fa45a206995ce604f830115921aa448591ae601
rrerusu <rrerusu@gmail.com>
2021-07-24 17:12:40 -0500

    Print values from file based on input, and nice table

---
+++ src/org/erusu/jhtp/chapter15/examples/MenuOption.java
@@ -0,0 +1,14 @@
+package org.erusu.jhtp.chapter15.examples;
+
+public enum MenuOption {
+    ZERO_BALANCE(1),
+    CREDIT_BALANCE(2),
+    DEBIT_BALANCE(3),
+    END(4);
+
+    private final int value;
+
+    private MenuOption(int value) {
+        this.value = value;
+    }
+}
