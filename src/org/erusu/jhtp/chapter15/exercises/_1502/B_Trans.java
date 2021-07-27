4e4cd5e135dec009da146cbc39e38ed068d6e3b7
rrerusu <rrerusu@gmail.com>
2021-07-27 17:22:59 -0500

    Read from file

---
+++ src/org/erusu/jhtp/chapter15/exercises/_1502/B_Trans.java
@@ -0,0 +1,16 @@
+package org.erusu.jhtp.chapter15.exercises._1502;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class B_Trans {
+    public static void main(String[] args) throws IOException{
+        Scanner inTransaction = new Scanner(Paths.get(
+            "src/org/erusu/jhtp/chapter15/exercises/_1502/trans.txt"
+        ));
+
+        System.out.printf("%s", inTransaction.nextLine());
+        inTransaction.close();
+    }
+}
