79b99095d75e1bbe774a45d78016608c79bf69ec
rrerusu <rrerusu@gmail.com>
2021-08-03 18:03:25 -0500

    Descending array sort as list

---
+++ src/org/erusu/jhtp/chapter16/examples/Sort2.java
@@ -0,0 +1,17 @@
+package org.erusu.jhtp.chapter16.examples;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort2 {
+    public static void main(String[] args) {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        List<String> list = Arrays.asList(suits);
+        System.out.printf("Unsorted array elements: %s\n", list);
+
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.printf("Sorted list elements: %s\n", list);
+    }
+}
