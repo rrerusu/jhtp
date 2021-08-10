9f877c6701fec589d3bb9da788e36847ff232d3e
rrerusu <rrerusu@gmail.com>
2021-08-09 20:00:05 -0500

    Collections addAll, frequency, and disjoint methods

---
+++ src/org/erusu/jhtp/chapter16/examples/Algorithms2.java
@@ -0,0 +1,40 @@
+package org.erusu.jhtp.chapter16.examples;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Algorithms2 {
+    public static void main(String[] args) {
+        String[] colors = {"red", "white", "yellow", "blue"};
+        List<String> list1 = Arrays.asList(colors);
+        ArrayList<String> list2 = new ArrayList<>();
+
+        list2.add("black");
+        list2.add("red");
+        list2.add("green");
+
+        System.out.printf("Before addAll, list2 contains: ");
+
+        for(String s : list2) {
+            System.out.printf("%s ", s);
+        }
+
+        Collections.addAll(list2, colors);
+
+        System.out.printf("\nAfter addAll, list2 contains: ");
+
+        for(String str : list2) {
+            System.out.printf("%s ", str);
+        }
+
+        int frequency = Collections.frequency(list2, "red");
+        System.out.printf("\nFrequency of red in list2: %d\n", frequency);
+
+        boolean disjoint = Collections.disjoint(list1, list2);
+
+        System.out.printf("list1 and list2 %s elements in common\n",
+            (disjoint ? "do not have" : "have"));
+    }
+}
