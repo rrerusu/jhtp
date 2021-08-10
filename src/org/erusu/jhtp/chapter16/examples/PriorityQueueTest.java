f5dda2542f6217e1da1c39a3de023ee1eeed0b26
rrerusu <rrerusu@gmail.com>
2021-08-09 20:45:36 -0500

    PriorityQueue methods

---
+++ src/org/erusu/jhtp/chapter16/examples/PriorityQueueTest.java
@@ -0,0 +1,20 @@
+package org.erusu.jhtp.chapter16.examples;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+        PriorityQueue<Double> queue = new PriorityQueue<>();
+
+        queue.offer(3.2);
+        queue.offer(9.8);
+        queue.offer(5.4);
+
+        System.out.printf("Polling from queue: ");
+
+        while(queue.size() > 0) {
+            System.out.printf("%.2f ", queue.peek());
+            queue.poll();
+        }
+    }
+}
