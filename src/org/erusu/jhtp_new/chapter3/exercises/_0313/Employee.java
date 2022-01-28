510cf6137bae4b7403d53f0c1913c9475ba264fa
rrerusu <rrerusu@gmail.com>
2022-01-28 16:52:23 -0600

    Employee class

---
+++ src/org/erusu/jhtp_new/chapter3/exercises/_0313/Employee.java
@@ -0,0 +1,37 @@
+package org.erusu.jhtp_new.chapter3.exercises._0313;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+
+    public Employee(String fname, String lname, double msalary) {
+        firstName = fname;
+        lastName = lname;
+        monthlySalary = msalary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setFirstName(String fname) {
+        firstName = fname;
+    }
+
+    public void setLastName(String lname) {
+        lastName = lname;
+    }
+
+    public void setMonthlySalary(double msalary) {
+        monthlySalary = (msalary > 0) ? msalary : 0;
+    }
+}
