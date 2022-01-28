758b3b2eb3d445025701ddfb7b606e019c28f0a1
rrerusu <rrerusu@gmail.com>
2022-01-28 12:45:46 -0600

    Include constructor that takes name of Account owner

--- src/org/erusu/jhtp_new/chapter3/examples/Account.java
+++ src/org/erusu/jhtp_new/chapter3/examples/Account.java
@@ -3,6 +3,11 @@ package org.erusu.jhtp_new.chapter3.examples;
 public class Account {
     private String name;
 
+    // 3.5 Update: Add Constructor
+    public Account(String name) {
+        this.name = name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
--- src/org/erusu/jhtp_new/chapter3/examples/AccountTest.java
+++ src/org/erusu/jhtp_new/chapter3/examples/AccountTest.java
@@ -1,22 +1,28 @@
 package org.erusu.jhtp_new.chapter3.examples;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class AccountTest {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
 
-        Account myAccount = new Account();
+        System.out.printf("account1 name is: %s\n", account1.getName());
+        System.out.printf("account2 name is: %s\n", account2.getName());
 
-        System.out.printf("Initial name: %s\n\n", myAccount.getName());
+        // Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter the name: ");
-        String name = input.nextLine();
-        myAccount.setName(name);
-        System.out.println();
+        // Account myAccount = new Account();
 
-        System.out.printf("Name in object myAccount is: \n%s\n", myAccount.getName());
+        // System.out.printf("Initial name: %s\n\n", myAccount.getName());
 
-        input.close();
+        // System.out.println("Please enter the name: ");
+        // String name = input.nextLine();
+        // myAccount.setName(name);
+        // System.out.println();
+
+        // System.out.printf("Name in object myAccount is: \n%s\n", myAccount.getName());
+
+        // input.close();
     }
 }
